eab6665b451a27567fc58efaa58fb604fbb0da93
davidyg <ozirus213@gmail.com>
2019-03-28 19:44:28 +0100

    Componente Buscador - 0.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { EtiquetasComponent } from './etiquetas/etiquetas.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { HttpClientModule } from '@angular/common/http';
+import { FiltroComponent } from './filtro/filtro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EtiquetasComponent
+    EtiquetasComponent,
+    FiltroComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +20,6 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent, EtiquetasComponent]
+  bootstrap: [AppComponent, EtiquetasComponent, FiltroComponent]
 })
 export class AppModule { }
